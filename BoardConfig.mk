447ff2f17c54c676fa8186819428ed6b64d2ee7d
Arne Coucheron <arco68@gmail.com>
2010-12-12 06:17:30 +0100

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,105 @@
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include device/htc/tattoo/BoardConfigVendor.mk
+
+# ARMv6-compatible processor rev 5 (v6l)
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_ARCH_VARIANT := armv6j
+TARGET_CPU_ABI := armeabi-v6j
+TARGET_CPU_ABI2 := armeabi
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+
+BOARD_LDPI_RECOVERY := true
+BOARD_RECOVERY_FONT_INCLUDE_HEADER := ../../device/htc/tattoo/font_7x16.h
+BOARD_RECOVERY_FONT_WIDTH := 7
+BOARD_RECOVERY_FONT_HEIGHT := 16
+
+TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/tattoo/custom/recovery_kernel
+TARGET_PREBUILT_KERNEL := device/htc/tattoo/custom/kernel
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libWifiApi
+BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/sta_dk_4_0_4_32
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_ARG      := ""
+WIFI_DRIVER_MODULE_NAME     := "wlan"
+WIFI_FIRMWARE_LOADER        := "wlan_loader"
+
+TARGET_BOOTLOADER_BOARD_NAME := bahamas
+
+TARGET_BOOTLOADER_LIBS := \
+	libboot_board_dream_sapphire_combined \
+	libboot_arch_msm7k \
+	libboot_arch_armv6
+
+TARGET_BOOTLOADER_LINK_SCRIPT := \
+	hardware/msm7k/boot/boot.ld
+
+TARGET_PROVIDES_INIT_RC := false
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x02E00000
+
+BOARD_USES_GENERIC_AUDIO := false
+
+BOARD_USE_HTC_LIBSENSORS := true
+
+# Use HTC USB Function Switch to enable tethering via USB
+BOARD_USE_HTC_USB_FUNCTION_SWITCH := true
+
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+
+BOARD_HAVE_BLUETOOTH := true
+
+BOARD_VENDOR_USE_AKMD := akm8973
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1355
+
+BOARD_HAS_LIMITED_EGL := true
+
+TARGET_HARDWARE_3D := false
+
+# OpenGL drivers config file path
+BOARD_EGL_CFG := device/htc/tattoo/egl.cfg
+
+# No authoring clock for OpenCore
+# BOARD_NO_PV_AUTHORING_CLOCK := true
+
+BOARD_USES_QCOM_LIBS := true
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+TARGET_OTA_ASSERT_DEVICE := bahamas
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BOOTLOADER_BOARD_NAME=bahamas
+
+# # cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 000a0000 00020000 "misc"
+# mtd1: 00500000 00020000 "recovery"
+# mtd2: 00280000 00020000 "boot"
+# mtd3: 09600000 00020000 "system"
+# mtd4: 09600000 00020000 "cache"
+# mtd5: 0a520000 00020000 "userdata"
+# Changed for Tattoo
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00280000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x09600000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0a520000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Stop compiling test_* binaries for eng tag
+STOP_TEST_BINS := true
---
+++ tattoo-vendor-blobs.mk
@@ -0,0 +1,72 @@
+# libgps is necessary to complete the compilation
+PRODUCT_COPY_FILES += \
+    device/htc/tattoo/custom/logo.rle:root/logo.rle \
+    device/htc/tattoo/proprietary/libgps.so:obj/lib/libgps.so
+
+# proprietary for modules
+PRODUCT_COPY_FILES += \
+	device/htc/tattoo/custom/modules.sqf:system/lib/modules/modules.sqf
+
+PRODUCT_COPY_FILES += \
+        device/htc/tattoo/custom/backuptool.sh:system/bin/backuptool.sh
+
+# proprietary for etc
+PRODUCT_COPY_FILES += \
+	device/htc/tattoo/custom/05mountsd:system/etc/init.d/05mountsd \
+        device/htc/tattoo/custom/10calibrate_screen:system/etc/init.d/10calibrate_screen
+
+# Tattoo Calibration apps
+PRODUCT_COPY_FILES += \
+    device/htc/tattoo/custom/app/HTCCalibrate.apk:system/app/HTCCalibrate.apk
+
+PRODUCT_COPY_FILES += \
+    device/htc/tattoo/custom/app/FancyWidget.apk:system/app/FancyWidget.apk
+
+# proprietary for /system/bin
+PRODUCT_COPY_FILES += \
+        device/htc/tattoo/custom/compcache:system/bin/compcache
+
+# proprietary for /system/usr/keychars and /system/usr/keylayout
+PRODUCT_COPY_FILES += \
+	device/htc/tattoo/proprietary/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+	device/htc/tattoo/proprietary/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin
+
+# Other bin stuff
+PRODUCT_COPY_FILES += \
+	device/htc/tattoo/proprietary/akm8973:system/bin/akm8973
+
+# proprietary stuff
+PRODUCT_COPY_FILES += \
+	device/htc/tattoo/proprietary/AudioPara4.csv:system/etc/AudioPara4.csv \
+	device/htc/tattoo/proprietary/AudioFilter.csv:system/etc/AudioFilter.csv \
+	device/htc/tattoo/proprietary/AudioPreProcess.csv:system/etc/AudioPreProcess.csv \
+	device/htc/tattoo/proprietary/agps_rm:/system/etc/agps_rm \
+	device/htc/tattoo/proprietary/tiwlan.ini:system/etc/wifi/tiwlan.ini \
+	device/htc/tattoo/proprietary/libaudioeq.so:system/lib/libaudioeq.so \
+	device/htc/tattoo/proprietary/libGLES_qcom.so:system/lib/egl/libGLES_qcom.so \
+	device/htc/tattoo/proprietary/libgps.so:system/lib/libgps.so \
+	device/htc/tattoo/proprietary/liboemcamera.so:system/lib/liboemcamera.so \
+	device/htc/tattoo/proprietary/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
+	device/htc/tattoo/proprietary/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
+	device/htc/tattoo/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
+	device/htc/tattoo/proprietary/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+	device/htc/tattoo/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+	device/htc/tattoo/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
+	device/htc/tattoo/proprietary/bahamas-keypad.kl:system/usr/keylayout/bahamas-keypad.kl \
+	device/htc/tattoo/proprietary/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+	device/htc/tattoo/proprietary/qwerty.kl:system/usr/keylayout/qwerty.kl \
+	device/htc/tattoo/proprietary/sensors.bahamas.so:system/lib/hw/sensors.bahamas.so \
+        device/htc/tattoo/firmware/Fw1251r1c.bin:system/etc/wifi/Fw1251r1c.bin \
+        device/htc/tattoo/firmware/brf6300.bin:system/etc/firmware/brf6300.bin \
+        device/htc/tattoo/firmware/brf6350.bin:system/etc/firmware/brf6350.bin
+
+ifdef WITH_WINDOWS_MEDIA
+PRODUCT_COPY_FILES += \
+    device/htc/tattoo/proprietary/libomx_wmadec_sharedlibrary.so:system/lib/libomx_wmadec_sharedlibrary.so \
+    device/htc/tattoo/proprietary/libomx_wmvdec_sharedlibrary.so:system/lib/libomx_wmvdec_sharedlibrary.so \
+    device/htc/tattoo/proprietary/libpvasfcommon.so:system/lib/libpvasfcommon.so \
+    device/htc/tattoo/proprietary/libpvasflocalpbreg.so:system/lib/libpvasflocalpbreg.so \
+    device/htc/tattoo/proprietary/libpvasflocalpb.so:system/lib/libpvasflocalpb.so \
+    device/htc/tattoo/proprietary/pvasflocal.cfg:system/etc/pvasflocal.cfg
+endif
+
