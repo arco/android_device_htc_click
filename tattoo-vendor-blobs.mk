7f33abc3b405f33b983ed28858c6092ec51a380e
Arne Coucheron <arco68@gmail.com>
2010-12-15 07:29:53 +0100

    Add some kernel tweaking parameters

--- tattoo-vendor-blobs.mk
+++ tattoo-vendor-blobs.mk
@@ -13,7 +13,8 @@ PRODUCT_COPY_FILES += \
 # proprietary for etc
 PRODUCT_COPY_FILES += \
     device/htc/tattoo/custom/05mountsd:system/etc/init.d/05mountsd \
-    device/htc/tattoo/custom/10calibrate_screen:system/etc/init.d/10calibrate_screen
+    device/htc/tattoo/custom/10calibrate_screen:system/etc/init.d/10calibrate_screen \
+    device/htc/tattoo/custom/sysctl.conf:system/etc/sysctl.conf
 
 # Tattoo Calibration apps
 PRODUCT_COPY_FILES += \
