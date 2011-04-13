910db8535e103cf653e8bd3ee1600e58a3c234f8
Arne Coucheron <arco68@gmail.com>
2011-04-13 07:16:43 +0200

    Add systemwide ldpi overlay to build, and bump version number

--- vendor/cyanogen/products/cyanogen_tattoo.mk
+++ vendor/cyanogen/products/cyanogen_tattoo.mk
@@ -14,15 +14,21 @@ PRODUCT_MODEL := HTC Tattoo
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_tattoo BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
 
+# Add LDPI assets, in addition to MDPI
+PRODUCT_LOCALES += ldpi mdpi
+
 # Extra Tattoo overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/tattoo
+
+# Extra overlay for LDPI
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
 # TI FM radio
 $(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
 
 # Set ro.modversion
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=nFinityGB-0.82
+    ro.modversion=nFinityGB-1.0
 
 # Copy tattoo specific prebuilt files
 PRODUCT_COPY_FILES +=  \
