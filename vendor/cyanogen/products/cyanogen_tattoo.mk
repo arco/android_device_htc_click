bcc3733822ede621abac3b29b3c02ab649bb9205
Arne Coucheron <arco68@gmail.com>
2011-05-04 07:14:49 +0200

    Update build_id and fingerprint for 2.3.4

--- vendor/cyanogen/products/cyanogen_tattoo.mk
+++ vendor/cyanogen/products/cyanogen_tattoo.mk
@@ -12,7 +12,7 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := tattoo
 PRODUCT_MODEL := HTC Tattoo
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_tattoo BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_tattoo BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
 
 # Add LDPI assets, in addition to MDPI
 PRODUCT_LOCALES += ldpi mdpi
@@ -28,7 +28,7 @@ $(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
 
 # Set ro.modversion
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=nFinityGB-1.0
+    ro.modversion=nFinityGB-1.02
 
 # Copy tattoo specific prebuilt files
 PRODUCT_COPY_FILES +=  \
